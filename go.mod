c6b57c9605fa75bd554f1d66e08e55b73b3e63e0
Lazaro Legina <lazarolegina@gmail.com>
2022-06-23 11:11:02 +0700

    major update say-hello v2 with parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/lazarolegina/go-say-hello
+module github.com/lazarolegina/go-say-hello/v2
 
 go 1.18
--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,5 @@
 package go_say_hello //pembuatan folder ternyata harus pake underscore "_". tidak boleh pake strip "-"
 
-func SayHello() string {
-	return "Hello v1.5.1"
+func SayHello(name string) string {
+	return "Hello" + name + "ini v2 yaa"
 }

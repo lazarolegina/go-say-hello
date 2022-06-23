7d90cfe04cfe22fe5a13868581b7f44ccf2f3dd5
Lazaro Legina <lazarolegina@gmail.com>
2022-06-23 11:44:43 +0700

    update v2.0.1 fix kata-kata

--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,5 @@
 package go_say_hello //pembuatan folder ternyata harus pake underscore "_". tidak boleh pake strip "-"
 
 func SayHello(name string) string {
-	return "Hello" + name + "ini v2 yaa"
+	return "Hello " + name + " ini v2 yaa"
 }
